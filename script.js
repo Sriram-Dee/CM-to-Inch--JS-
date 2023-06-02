812f2f9ef3806e38597dfa06c710a6f402c21e9d
SRIRAM R <you@example.com>
2023-06-02 23:07:52 +0530

    updated

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-function convert(){
-    let cmVal = Number(document.getElementById("input").value)
-    let inchVal = cmVal / 2.54
-    let result = document.getElementById("result")
-    result.innerHTML = inchVal.toFixed(3) + " Inches"
-}
+
